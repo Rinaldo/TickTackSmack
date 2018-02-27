1497b956e0de9672d78ae0a523a6ca26c67be0d8
Robert Rinaldo <rinaldo646@gmail.com>
2018-02-26 23:30:21 -0500

    added redirect to hard mode

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
 import Header from './Header.jsx'
 import ModeChoice from './ModeChoice.jsx'
@@ -12,7 +12,7 @@ const Main = props => {
       <div className={`main main-${props.mode}`}>
         <Header />
         <div className="content">
-          <Route exact path="/" component={ModeChoice} />
+          <Route exact path="/" render={() => <Redirect to="/hard" />} />
           <Route exact path="/easy" component={ModeChoice} />
           <Route exact path="/hard" component={ModeChoice} />
           <Route exact path="/smackdown" component={ModeChoice} />
