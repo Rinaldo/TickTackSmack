6813c66d62e7ee6e402309f6fd51e41b50b297f5
Robert Rinaldo <rinaldo646@gmail.com>
2018-04-11 14:31:32 -0400

    refactored game engine to make it more functional

--- app/gameEngine.js
+++ app/gameEngine.js
@@ -1,5 +1,5 @@
 import store from './store'
-import Immutable, { List, equals } from 'immutable'
+import Immutable, { List } from 'immutable'
 import { updateBoard, swapFriendFoe, completedGame, declareWinner, setMode, setPlayer, resetGame } from './reducers/game'
 
 // points for calculating row scores
@@ -21,18 +21,22 @@ const getRandomElement = array => {
   return array[Math.floor((Math.random() * array.length))]
 }
 
-// calculates row scores by multiplying the point values of the cells in each row
-const calculateRowScores = () => {
-
+// maps board of x's and o's to board of friend, foe, and blank points
+const getPointsBoard = () => {
   const gameState = store.getState().get('gameState')
   const board = gameState.get('board')
-  const pointsBoard = board.map(cell => {
+  return board.map(cell => {
     if (cell === gameState.get('friend')) return friendPts
     if (cell === gameState.get('foe')) return foePts
     else return blankPts
   })
-  // specific cells that make up each row
-  const rowScores = List([
+}
+
+// set mixed (friend, foe, and blank) rows to the mixedPts constant
+const fixMixedRows = rowScores => rowScores.map(rowPts => (rowPts === friendPts * foePts ? mixedPts : rowPts))
+
+// specific cells that make up each row
+const pointsBoardToRowScores = pointsBoard => List([
   pointsBoard.get(0) * pointsBoard.get(1) * pointsBoard.get(2),
   pointsBoard.get(3) * pointsBoard.get(4) * pointsBoard.get(5),
   pointsBoard.get(6) * pointsBoard.get(7) * pointsBoard.get(8),
@@ -42,44 +46,58 @@ const calculateRowScores = () => {
   pointsBoard.get(0) * pointsBoard.get(4) * pointsBoard.get(8),
   pointsBoard.get(2) * pointsBoard.get(4) * pointsBoard.get(6),
   ])
-  // set mixed (friend, foe, and blank) rows to the mixedPts constant
-  const fixedScores = rowScores.map(rowPts => (rowPts === friendPts * foePts ? mixedPts : rowPts))
 
+// calculates row scores by multiplying the point values of the cells in each row
+const calculateRowScores = () => {
+  const pointsBoard = getPointsBoard()
+  const rowScores = pointsBoardToRowScores(pointsBoard)
+  const fixedScores = fixMixedRows(rowScores)
   return fixedScores
+}
+
+const fixEdgeCases = (rowScores, board) => {
+
+  const numTurns = board.filter(Boolean).size
+  // provides correct scoring to maximize chance of creating fork for opponent
+  if (numTurns === 2 && rowScores.sort().equals(edgeCase0)) {
+    rowScores = rowScores.get(6) === 0 ?
+      rowScores.set(6, rowScores.get(6) + friendPts + foePts) :
+      rowScores.set(7, rowScores.get(7) + friendPts + foePts)
+  }
+  // fixes scoring if computer is on receiving end of fork attempt
+  else if (numTurns === 3 && rowScores.sort().equals(edgeCase1)) {
+    rowScores = rowScores.set(1, rowScores.get(1) + friendPts)
+    rowScores = rowScores.set(4, rowScores.get(4) + friendPts)
+  }
+  return rowScores
 }
 
 // calculates cell scores by summing the row scores of the rows the cell is in
 const calculateCellScores = rowScores => {
-
   const board = store.getState().getIn(['gameState', 'board'])
-  const numTurns = board.filter(Boolean).size
+  const fixedScores = fixEdgeCases(rowScores, board)
 
-  // provides correct scoring to maximize chance of creating fork for opponent
-  if (numTurns === 2 && rowScores.sort().equals(edgeCase0)) {
-    if (rowScores.get(6) === 0) rowScores = rowScores.set(6, rowScores.get(6) + friendPts + foePts)
-    else rowScores = rowScores.set(7, rowScores.get(7) + friendPts + foePts)
-  }
-  // fixes scoring if computer is on receiving end of fork attempt
-  if (numTurns === 3 && rowScores.sort().equals(edgeCase1)) {
-    rowScores = rowScores.set(1, rowScores.get(1) + friendPts)
-    rowScores = rowScores.set(4, rowScores.get(4) + friendPts)
-  }
-  const cellScores = board.map((cell, index) => {
-    return cell ? -1 : cellRows.get(index).reduce((accum, currVal) => accum + rowScores.get(currVal), 0)
+  return board.map((cell, index) => {
+    return cell ? -1 : cellRows.get(index).reduce((accum, currVal) => accum + fixedScores.get(currVal), 0)
   })
-
-  return cellScores
 }
 
 const updateCompletionStatus = () => {
 
+  const gameState = store.getState().get('gameState')
+  const friend = gameState.get('friend')
+  const foe = gameState.get('foe')
+
   const rowScores = calculateRowScores()
-  if (rowScores.indexOf(Math.pow(friendPts, 3)) !== -1) {
+  const threeFriendsInARow = rowScores.includes(Math.pow(friendPts, 3))
+  const threeFoesInARow = rowScores.includes(Math.pow(foePts, 3))
+
+  if (threeFriendsInARow) {
     store.dispatch(completedGame())
-    store.dispatch(declareWinner(store.getState().getIn(['gameState', 'friend'])))
-  } else if (rowScores.indexOf(Math.pow(foePts, 3)) !== -1) {
+    store.dispatch(declareWinner(friend))
+  } else if (threeFoesInARow) {
     store.dispatch(completedGame())
-    store.dispatch(declareWinner(store.getState().getIn(['gameState', 'foe'])))
+    store.dispatch(declareWinner(foe))
   }
   const cellScores = calculateCellScores(rowScores)
 
@@ -102,7 +120,7 @@ const choose = () => {
   return candidates
 }
 
-// hard coded bad moves for the first 3 turns, otherwise plays as normal
+// hard coded bad moves for the first 3 turns
 const goEasy = (gameState, numTurns) => {
 
   const board = gameState.get('board')
@@ -122,8 +140,9 @@ const goEasy = (gameState, numTurns) => {
     } else if (board.get(5) === gameState.get('friend') && !board.get(3)) {
       return 3
     }
+  } else {
+    return getRandomElement(choose())
   }
-  return getRandomElement(choose())
 }
 
 const go = () => {
@@ -132,14 +151,12 @@ const go = () => {
   const numTurns = gameState.get('board').filter(Boolean).size
   let choice
 
-  if (gameState.get('mode') === 'easy') {
+  if (gameState.get('mode') === 'easy' && numTurns < 3) {
     choice = goEasy(gameState, numTurns)
   } else if (numTurns === 0) {
-    if (gameState.get('mode') === 'smackdown') {
-      choice = getRandomElement([0, 2, 6, 8])
-    } else {
-      choice = getRandomElement([0, 1, 2, 3, 4, 5, 6, 7, 8])
-    }
+    choice = gameState.get('mode') === 'smackdown' ?
+      getRandomElement([0, 2, 6, 8]) :
+      getRandomElement([0, 1, 2, 3, 4, 5, 6, 7, 8])
   } else {
     choice = getRandomElement(choose())
   }
