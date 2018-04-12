a8c6b72c4807483211b0e38f104d915d13559159
Robert Rinaldo <rinaldo646@gmail.com>
2018-04-12 15:14:40 -0400

    dev server configured properly

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,8 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   devtool: 'inline-source-map',
-  // devServer: {
-  //   contentBase: './public'
-  // }
+  devServer: {
+    contentBase: './public',
+    historyApiFallback: true,
+  }
 })
